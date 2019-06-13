7aadf3aeb5efeb8d1370f3742427d43c3afc4b1d
Vuhuy Pham <vuhuypham99@gmail.com>
2019-06-12 23:32:04 -0700

    Add more voohoo-photo build files

---
+++ precache-manifest.d8fe5a4ff2af3fdfd6091ffc772a8739.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a2bab8298bfec16e053add392f0022",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2bb7005ac2d455a30ef",
+    "url": "/static/css/main.3d6988e9.chunk.css"
+  },
+  {
+    "revision": "68c5b389837833bf7d07",
+    "url": "/static/js/2.b64b1708.chunk.js"
+  },
+  {
+    "revision": "b2bb7005ac2d455a30ef",
+    "url": "/static/js/main.3b2045c9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
